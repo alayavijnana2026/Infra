225aa8575ba03981ff138f42a08189db3fa46b22
anson <anson@example.com>
2026-06-28 01:41:43 +0700

    fix: change sysctl.conf file

--- system/init.sh
+++ system/init.sh
@@ -361,17 +361,33 @@ configure_sysctl() {
     log "Configure Sysctl"
 cat >/etc/sysctl.d/99-custom.conf <<EOF
 fs.file-max = 2097152
+fs.nr_open = 2097152
 net.core.somaxconn = 65535
-net.core.netdev_max_backlog = 65535
+net.core.netdev_max_backlog = 250000
+net.ipv4.tcp_max_syn_backlog = 65535
 net.ipv4.ip_local_port_range = 1024 65535
-net.ipv4.tcp_max_syn_backlog = 8192
-net.ipv4.tcp_fin_timeout = 15
 net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_fin_timeout = 15
+net.ipv4.tcp_max_tw_buckets = 2000000
+net.ipv4.tcp_keepalive_time = 300
 net.ipv4.tcp_keepalive_intvl = 30
 net.ipv4.tcp_keepalive_probes = 5
+net.core.rmem_default = 262144
+net.core.wmem_default = 262144
+net.core.rmem_max = 134217728
+net.core.wmem_max = 134217728
+net.core.optmem_max = 25165824
+net.ipv4.tcp_rmem = 4096 87380 134217728
+net.ipv4.tcp_wmem = 4096 65536 134217728
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_max_orphans = 262144
 vm.swappiness = 10
-vm.max_map_count = 262144
+kernel.pid_max = 4194304
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_slow_start_after_idle = 0
 EOF
     sysctl --system
 }
